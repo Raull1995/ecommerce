088a165023d6f1e49068636220da50687d69e0f6
Raul ALencar <raulsilvaa2@gmail.com>
2018-05-07 09:28:18 -0300

    criamos a classe Page

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,24 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+              
+    </head>    
+    
+    <body>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+    
+    
+    
+    
+    
+</html>
